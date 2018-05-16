5518dabdc2ca6509d5de0d880c784d5e5297cb75
Kate Lukashick <lukashick.kate@gmail.com>
2018-05-16 15:02:54 +0300

    add license

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+/*
+ * *******************************************************************************
+ *   Copyright (c) 2018 Edgeworx, Inc.
+ *
+ *   This program and the accompanying materials are made available under the
+ *   terms of the Eclipse Public License v. 2.0 which is available at
+ *   http://www.eclipse.org/legal/epl-2.0
+ *
+ *   SPDX-License-Identifier: EPL-2.0
+ * *******************************************************************************
+ */
+
 /**
  * This module lets you easily build an ioElement.
  * It gives you all the functionality to interact with ioFog via Local API.
